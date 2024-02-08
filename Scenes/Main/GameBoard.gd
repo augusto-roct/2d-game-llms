24e43dd14b04c59aff5bbf7f261c82ed42c59819
augusto-roct <augusto.roct@gmail.com>
2024-02-08 08:50:15 -0300

    add: prompt farmer

---
+++ Scenes/Main/GameBoard.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+var prompt_farmer = FileAccess.open("./Scenes/Main/prompts/farmer.txt", FileAccess.READ).get_as_text()
+
+func _ready():
+	$Joseph.start(Vector2(1200, 80), "Joseph", prompt_farmer)
+	$Will.start(Vector2(80, 640), "Will")
+	
+
+
+func _process(delta):
+	pass
--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -5,6 +5,10 @@ var screen_size
 var is_animating = false
 var select_movement
 var player_size
+var headers = ["Content-Type: application/json"]
+var url = "http://127.0.0.1:8000/chat/completion"
+var name_player
+var system_prompt
 
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -51,15 +55,44 @@ func _process(delta):
 		$AnimatedSprite2D.animation = "up"
 	elif velocity.y > 0:
 		$AnimatedSprite2D.animation = "down"
+	
+	var data = {
+	  "text": "Oi",
+	  "system": system_prompt,
+	  "parameters": {},
+	  "generation_config": {
+		"candidate_count": 1,
+		"max_output_tokens": 512,
+		"temperature": 0.3,
+		"top_p": 0.1,
+		"top_k": 1
+	  },
+	  "safety_settings": {
+		"HARM_CATEGORY_HARASSMENT": "BLOCK_NONE",
+		"HARM_CATEGORY_HATE_SPEECH": "BLOCK_NONE",
+		"HARM_CATEGORY_SEXUALLY_EXPLICIT": "BLOCK_NONE",
+		"HARM_CATEGORY_DANGEROUS_CONTENT": "BLOCK_NONE"
+	  }
+	}
+	var payload = JSON.stringify(data)
+	
+	$HTTPRequest.request(url, headers, HTTPClient.METHOD_POST, payload)
 
 
 func _on_wait_animation_timer_timeout():
 	is_animating = false
 
 
-func start(pos, name_player):
+func _on_http_request_completed(result, response_code, headers, body):
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	print(json)
+
+
+func start(pos, nick, prompt):
 	position = pos
 	
+	name_player = nick
+	system_prompt = prompt.replace("{name_player}", name_player)
 	$HUD/NameLabel.text = name_player
 	
 	$HUD/NameLabel.position = pos
@@ -67,3 +100,4 @@ func start(pos, name_player):
 	$HUD/NameLabel.position.x = pos.x - (player_size.x * 2.5)
 	$HUD/NameLabel.show()
 	
+	system_prompt = prompt
